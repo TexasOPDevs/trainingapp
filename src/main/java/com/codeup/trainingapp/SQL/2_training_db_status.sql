69049167810ed32f779239e16f3f0c72417e5d3d
samuelmoore <samuel.c.moore1221@gmail.com>
2019-07-16 14:10:04 -0500

    added mysql inserts

---
+++ src/main/java/com/codeup/trainingapp/SQL/2_training_db_status.sql
@@ -0,0 +1,5 @@
+INSERT INTO training_db.status (id, name) VALUES (1, 'pending');
+INSERT INTO training_db.status (id, name) VALUES (2, 'approved');
+INSERT INTO training_db.status (id, name) VALUES (3, 'rejected');
+INSERT INTO training_db.status (id, name) VALUES (4, 'open');
+INSERT INTO training_db.status (id, name) VALUES (5, 'closed');
--- src/main/java/com/codeup/trainingapp/models/Needs/Attendance.java
+++ src/main/java/com/codeup/trainingapp/models/Needs/Attendance.java
@@ -11,6 +11,7 @@ import java.sql.Date;
 public class Attendance {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     @Column
