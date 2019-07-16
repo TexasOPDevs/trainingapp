d5fee9ea44c96cf116ab8a5671586e5fd515462c
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-07-16 12:17:03 -0500

    working student portal

--- src/main/java/com/codeup/trainingapp/Repositories/ProviderRepository.java
+++ src/main/java/com/codeup/trainingapp/Repositories/ProviderRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProviderRepository extends CrudRepository<Provider, Long> {
 
+
+    Provider findByName(String name);
 }
