169ccbe938341c7312ff3580bc40fa4d5a5797d4
lindsey.jason.andrew@gmail.com <lindsey.jason.andrew@gmail.com>
2019-07-24 11:19:30 -0500

    logo in tables

--- src/main/resources/static/js/coord-profile.js
+++ src/main/resources/static/js/coord-profile.js
@@ -7,7 +7,18 @@
     request.done(function (curricula) {
 
         console.log(curricula);
-        var html = '<table class="highlight"><thead><tr><th>Course Name</th><th class="hide-on-med-and-down">Creation Date</th><th class="hide-on-med-and-down">Materials</th><th></th></tr></thead><tbody>';
+        var html = html += `<div class="row">
+        <div class="col s12">
+            <div class="card white">
+                <div class="card-content">
+                    <img src="/toplogoblue.png" alt="logo" style="height: 150px; width: 150px; opacity:.15; position: absolute;
+                    top: 1%; left: 1%;"/>
+                    <h3 class="card-title center">Courses</h3>
+                    <div class="row">
+                        <div class="col s3">
+                        </div>
+                    </div>`;
+        html += '<table class="highlight"><thead><tr><th>Course Name</th><th class="hide-on-med-and-down">Creation Date</th><th class="hide-on-med-and-down">Materials</th><th></th></tr></thead><tbody>';
         curricula.forEach(function (cur) {
             cur.creationDate = dateShift(cur.creationDate);
             html += `<tr><td>${cur.name}</td><td class="hide-on-med-and-down">${cur.creationDate}</td><td class="hide-on-med-and-down">`;
@@ -19,10 +30,10 @@
                 <div class="modal-content">
                   <h4>${cur.name}</h4>
                   <h5>Learning Objectives:</h5>
-                  <pre><p>${cur.learning_objectives}</p></pre>
+                  <pre style="font: inherit;"><p>${cur.learning_objectives}</p></pre>
                   <h5>Description:</h5>
                   <p>${cur.description}</p>
-                  <a href="">View all active classes</a>
+                  <a th:href="@{}">View all active classes</a>
                 </div>
                 <div class="modal-footer">
                   <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
@@ -37,7 +48,7 @@
 })(jQuery);
 
 
- function provider($) {
+function provider($) {
 
     var request = $.ajax({
         'url': '/provider.json'
@@ -64,12 +75,12 @@
         </div>
         </h3>`;
 
-        html+=`<div class="row">
+        html += `<div class="row">
         <div class="col s12">
             <div class="card white">
                 <div class="card-content">
-                    <img src="/toplogoblue.png" alt="logo" style="height: 100em; width: 100em; position: absolute;
-                    top: 50%; left: 50%;"/>
+                    <img src="/toplogoblue.png" alt="logo" style="height: 150px; width: 150px; opacity:.15; position: absolute;
+                    top: 1%; left: 1%;"/>
                     <h3 class="card-title center">Courses</h3>
                     <div class="row">
                         <div class="col s3">
@@ -89,22 +100,22 @@
     });
 }
 
-function dateShift(str){
-     str=str.split("-");
-     let year =str.shift();
-     str.push(year);
-     str=str.join("-");
-     return str;
+function dateShift(str) {
+    str = str.split("-");
+    let year = str.shift();
+    str.push(year);
+    str = str.join("-");
+    return str;
 }
 
 provider($);
 
-function addListener(){
-    $('.makeinstructor').mousedown(function(e){
+function addListener() {
+    $('.makeinstructor').mousedown(function (e) {
         $(this).attr("disabled", true);
         e.preventDefault();
         /*  Submit form using Ajax */
-        var emp_id =($(this).prop("value"));
+        var emp_id = ($(this).prop("value"));
         //Prevent default submission of form
         //event delegate
         // e.preventDefault();
@@ -115,16 +126,15 @@ function addListener(){
         console.log(token);
         console.log(header);
         console.log(emp_id);
-        var data= emp_id;
+        var data = emp_id;
         //Remove all errors
-
 
 
         $.post({
             url: '/makeInstructor/' + emp_id,
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(data),
-            beforeSend: function(jqXHR){
+            beforeSend: function (jqXHR) {
                 jqXHR.setRequestHeader('X-CSRF-Token', token,)
             },
             dataType: "json",
@@ -134,7 +144,7 @@ function addListener(){
         });
         provider($)
     });
-    $('.makeinstructor').mouseup(function(e){
+    $('.makeinstructor').mouseup(function (e) {
         e.preventDefault();
         provider($);
     })
@@ -142,7 +152,7 @@ function addListener(){
 }
 
 
- // var makeinstructor = function($)
+// var makeinstructor = function($)
 
 
 (function ($) {
@@ -153,25 +163,35 @@ function addListener(){
     request.done(function (curricula) {
 
         console.log(curricula);
-        var html = '<table class="highlight"><thead><tr><th>Course Name</th><th class="hide-on-med-and-down">Location</th><th class="hide-on-med-and-down">Start Date</th><th class="hide-on-med-and-down">End Date</th><th class="hide-on-med-and-down">Status</th><th>Instructors</th></tr></thead><tbody>';
+        var html = html += `<div class="row">
+        <div class="col s12">
+            <div class="card white">
+                <div class="card-content">
+                    <img src="/toplogoblue.png" alt="logo" style="height: 150px; width: 150px; opacity:.15; position: absolute;
+                    top: 1%; left: 1%;"/>
+                    <h3 class="card-title center">Courses</h3>
+                    <div class="row">
+                        <div class="col s3">
+                        </div>
+                    </div>`;
+        html += '<table class="highlight"><thead><tr><th>Course Name</th><th class="hide-on-med-and-down">Location</th><th class="hide-on-med-and-down">Start Date</th><th class="hide-on-med-and-down">End Date</th><th class="hide-on-med-and-down">Status</th><th>Instructors</th></tr></thead><tbody>';
         curricula.forEach(function (cur) {
             cur.courses.forEach(function (course) {
 
 
                 if (course.startDate === null) {
                     course.startDate = "tbd";
-                } else{
-                    course.startDate=dateShift(course.startDate);
+                } else {
+                    course.startDate = dateShift(course.startDate);
                 }
                 if (course.endDate === null) {
                     course.endDate = "tbd";
-                } else{
+                } else {
                     course.endDate = dateShift(course.endDate);
                 }
                 if (course.location === null) {
                     course.location = "tbd";
                 }
-
 
 
                 html += `<tr><td>${cur.name}</td><td class="hide-on-med-and-down">${course.location}</td><td class="hide-on-med-and-down">${course.startDate}</td><td class="hide-on-med-and-down">${course.endDate}</td><td class="hide-on-med-and-down">${course.status.name}</td><td>`;
